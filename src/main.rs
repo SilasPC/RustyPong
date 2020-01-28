be4e7680400d282c3c6608fd8a06601bb88d13a7
SilasPC <pockendahl@hotmail.com>
2020-01-28 17:34:08 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,115 @@
+
+extern crate sdl2;
+extern crate gl;
+
+pub mod render_gl;
+
+// TODO: VAO, then create structs for VAO and VBO
+
+fn main() {
+	let sdl = sdl2::init().unwrap();
+	let video_subsystem = sdl.video().unwrap();
+	
+	let gl_attr = video_subsystem.gl_attr();
+	
+	gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+	gl_attr.set_context_version(4,5);
+	
+	let window = video_subsystem
+		.window("Rust game", 720,480)
+		.opengl()
+		.resizable()
+		.build()
+		.unwrap();
+	
+	let gl_ctx = window.gl_create_context().unwrap();
+	let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+	
+	unsafe {
+		gl::Viewport(0,0,720,480);
+		gl::ClearColor(0.3,0.3,0.5,1.0);
+	}
+	
+	use std::ffi::CString;
+	
+	let vert_shader = render_gl::Shader::from_vert_source(
+		&CString::new(include_str!("triangle.vert")).unwrap()
+	).unwrap();
+	
+	let frag_shader = render_gl::Shader::from_frag_source(
+		&CString::new(include_str!("triangle.frag")).unwrap()
+	).unwrap();
+	
+	let shader_program = render_gl::Program::from_shaders(
+		&[vert_shader, frag_shader]
+	).unwrap();
+	
+	shader_program.set_used();
+	
+	let verticies: Vec<f32> = vec![
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0
+	];
+	
+	let mut vbo: gl::types::GLuint = 0;
+	unsafe {
+		gl::GenBuffers(1, &mut vbo);
+	}
+	
+	unsafe {
+		gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+		gl::BufferData(
+			gl::ARRAY_BUFFER,
+			(verticies.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+			verticies.as_ptr() as *const gl::types::GLvoid,
+			gl::STATIC_DRAW,
+		);
+		gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+	}
+	
+	let mut vao: gl::types::GLuint = 0;
+	unsafe {
+		gl::GenVertexArrays(1, &mut vao);
+	}
+	
+	unsafe {
+		gl::BindVertexArray(vao);
+		
+		gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+		
+		gl::EnableVertexAttribArray(0);
+		gl::VertexAttribPointer(
+			0, // index
+			3, // nu. components (?)
+			gl::FLOAT, // datatype
+			gl::FALSE, // normalized int-to-float conversion
+			(3 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride
+			std::ptr::null() // offset
+		);
+		
+		gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+		gl::BindVertexArray(0);
+	}
+	
+	let mut event_pump = sdl.event_pump().unwrap();
+	'main: loop {
+		for event in event_pump.poll_iter() {
+			match event {
+				sdl2::event::Event::Quit {..} => break 'main,
+				_ => {}
+			}
+		}
+		shader_program.set_used();
+		unsafe {
+			gl::Clear(gl::COLOR_BUFFER_BIT);
+			gl::BindVertexArray(vao);
+			gl::DrawArrays(
+				gl::TRIANGLES,
+				0,
+				3
+			);
+		}
+		window.gl_swap_window();
+	}
+}
